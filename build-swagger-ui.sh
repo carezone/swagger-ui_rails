9a6babc429e1708a1c49de6dc8257c8ed3a6d03c
aurelian <oancea@gmail.com>
2014-11-27 15:56:49 +0100

    versions

--- build-swagger-ui.sh
+++ build-swagger-ui.sh
@@ -3,21 +3,23 @@
 patches=`pwd`/swagger-ui-patches/*.diff
 
 cd swagger-ui-src
-npm install
-
+# clean this
+rm -rf dist
+rm -rf node_modules
+# apply patches
 for f in $patches
 do
   echo "--> patching $f"
   git apply ${f}
 done
-
-rm -rf dist/
+# do the npm
+npm install
 npm run-script build
+# expose it to rails
 git status
 cd ../
-
 rake sync_swagger_ui
-
+# reset as it was before
 cd swagger-ui-src
 git reset --hard
 cd ../
